024babe50369a33002b2fe45fb942aef07f9451a
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-04-29 13:30:30 +0200

    Added script to fade on enter

---
+++ Assets/Scripts/FadingSprite.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadingSprite : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    public float alpha = 0.2f;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered");
+            _spriteRenderer.color = new Color(1f, 1f, 1f, alpha);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player exited");
+            _spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+
+        }
+    }
+}
