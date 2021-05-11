c7dc6f5b706d40ef774656aa3fb1688eec7ac5bf
Jonas-Ekman <82997350+Jonas-Ekman@users.noreply.github.com>
2021-05-11 22:04:38 +0200

    Fixed moving foliage

--- Assets/Scripts/FoliageMover.cs
+++ Assets/Scripts/FoliageMover.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class FoliageMover : MonoBehaviour
 {
+    public AudioClip[] foliageRustle;
     private Animator anim;
+    private AudioSource audioS;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        audioS = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,6 +21,10 @@ public class FoliageMover : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             anim.SetTrigger("move");
+            audioS.pitch = Random.Range(0.6f, 1.5f);
+            audioS.volume = Random.Range(0.6f, 1.4f);
+            int r = Random.Range(0, foliageRustle.Length);
+            audioS.PlayOneShot(foliageRustle[r]);
         }
     }
 
