f33cdddb29ed3f20688f54c486cbc4fbf6b38474
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-05-16 17:45:17 +0200

    Tooltip system added

---
+++ Assets/Scripts/TooltipTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerClickHandler
+{
+    public string[] storyElements;
+    public float timeToShow = 3f;
+
+    private int stringToShow = 0;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            if (stringToShow < storyElements.Length)
+            {
+                string content = storyElements[stringToShow];
+                TooltipSystem.Show(content);
+                stringToShow++;
+                // StartCoroutine("Delay");
+                Debug.Log("Right button pressed.");
+            } else
+                TooltipSystem.Hide();
+        }
+    }
+
+    /*
+    public void OnMouseExit()
+    {
+        TooltipSystem.Hide();
+    }
+    */
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(timeToShow); // disable tooltip after x secs
+        TooltipSystem.Hide();
+        Debug.Log("Hiding tooltip");
+    }
+}
