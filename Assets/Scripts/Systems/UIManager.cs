ac62141467acf8e9f9e0f50781cb706987d9b935
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-06-01 22:30:23 +0200

    Pause menu added

--- Assets/Scripts/Systems/UIManager.cs
+++ Assets/Scripts/Systems/UIManager.cs
@@ -5,10 +5,52 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject deathPanel;
+    [SerializeField] GameObject pausePanel;
+
+    public bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
 
     public void ToggleDeathPanel()
     {
         Debug.Log("Toggling deathpanel");
         deathPanel.SetActive(!deathPanel.activeSelf);
+    }
+
+    public void TogglePausePanel()
+    {
+        Debug.Log("Toggling pausepanel");
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        isPaused = pausePanel.activeSelf;
+        Debug.Log("Toggle and isPaused " + isPaused);
+    }
+
+    public void PauseGame()
+    {
+        TogglePausePanel();
+        Time.timeScale = 0f; // stops all animations, updates etc
+        Debug.Log("Pause called");
+        Debug.Log("isPaused " + isPaused);
+    }
+
+    public void ResumeGame()
+    {
+        TogglePausePanel();
+        Time.timeScale = 1f;
+        Debug.Log("Resume called");
+        Debug.Log("isPaused " + isPaused);
     }
 }
